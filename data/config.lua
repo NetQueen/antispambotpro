f08866501acc0ed6cd3976fa482f66e3efb4edd2
TeleAgentRobot <kingagent47.ar@gmail.com>
2016-03-23 18:16:42 +0430

    up

--- data/config.lua
+++ data/config.lua
@@ -76,7 +76,7 @@ do local _ = {
     "lea",
     "voice1"
   },
-  help_text = [[⭕️لیست دستورات مدیریت گروه:
+  help_text = [[ ⭕️لیست دستورات مدیریت گروه:
 
 حذف کردن کاربر ⬅️ اخراج [آیدی،کد،ریپلای]
 
@@ -147,7 +147,7 @@ do local _ = {
 
 فیلتر کلمه ⬅️ filter + کلمه
 
-حدف فیلتر ⬅️ filter - کلمه
+حذف فیلتر ⬅️ filter - کلمه
 
 اخطار کردن کلمه ⬅️ filter < کلمه
 
@@ -192,7 +192,7 @@ do local _ = {
 دریافت راهنمای ربات ↔️ راهنما
 —------------------------------—
 
-● TeleAgent Bot ● V2]],
+● TeleAgent Bot ● V2 ]],
   help_text_realm = "Realm Commands:\n\n!creategroup [Name]\nCreate a group\n\n!createrealm [Name]\nCreate a realm\n\n!setname [Name]\nSet realm name\n\n!setabout [GroupID] [Text]\nSet a group's about text\n\n!setrules [GroupID] [Text]\nSet a group's rules\n\n!lock [GroupID] [setting]\nLock a group's setting\n\n!unlock [GroupID] [setting]\nUnock a group's setting\n\n!wholist\nGet a list of members in group/realm\n\n!who\nGet a file of members in group/realm\n\n!type\nGet group type\n\n!kill chat [GroupID]\nKick all memebers and delete group\n\n!kill realm [RealmID]\nKick all members and delete realm\n\n!addadmin [id|username]\nPromote an admin by id OR username *Sudo only\n\n!removeadmin [id|username]\nDemote an admin by id OR username *Sudo only\n\n!list groups\nGet a list of all groups\n\n!list realms\nGet a list of all realms\n\n!log\nGrt a logfile of current group or realm\n\n!broadcast [text]\n!broadcast Hello !\nSend text to all groups\nOnly sudo users can run this command\n\n!bc [group_id] [text]\n!bc 123456789 Hello !\nThis command will send text to [group_id]\n\n\n**U can use both \"/\" and \"!\" \n\n\n*Only admins and sudo can add bots in group\n\n\n*Only admins and sudo can use kick,ban,unban,newlink,setphoto,setname,lock,unlock,set rules,set about and settings commands\n\n*Only admins and sudo can use res, setowner, commands\n",
   moderation = {
     data = "data/moderation.json"
