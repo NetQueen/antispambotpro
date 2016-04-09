1bd43cab22540e186a0142240e820fc42d701106
araeisy <teleagentteam@gmail.com>
2016-04-09 14:03:28 +0430

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -292,8 +292,7 @@ local function show_group_settingsmod(msg, data, target)
     	welcome = data[tostring(msg.to.id)]['settings']['welcome']
    	end
   local settings = data[tostring(target)]['settings']
-  local text = "تنظیمات گروه:\n⚙⚙⚙⚙\n>قفل نام گروه : "..settings.lock_name.."\n>قفل عکس گروه : "..settings.lock_photo.."\n>قفل اعضا : "..settings.lock_member.."\n>ممنوعیت ارسال لینک : "..lock_link.."\n>قفل ورود : "..lock_join.."\n>قفل رسانه : "..lock_media.."\n>قفل اشتراک گذاری : "..lock_share.."\n>حساسیت اسپم : "..NUM_MSG_MAX.."\n>قفل ربات ها : "..bots_protection.."\n>خوش امد : "..welcome.."\n>قفل تگ : "..tag.."\n>قفل اینگلیسی :"..lock_english.."\n>قفل فحش : "..lock_badw.."\n>مدل گروه : "..groupmodel.."\n>ورژن : "..version  return text
-end
+  local text = "تنظیمات گروه:\n⚙⚙⚙⚙\n⚙ قفل نام گروه : "..settings.lock_name.."\n⚙ قفل عکس گروه : "..settings.lock_photo.."\n⚙ قفل خروج : "..leave_ban.."\n⚙ ممنوعیت ارسال لینک : "..lock_link.."\n⚙ قفل ورود : "..lock_join.."\n⚙ قفل رسانه : "..lock_media.."\n⚙ قفل اشتراک گذاری : "..lock_share.."\n⚙ حساسیت اسپم : "..NUM_MSG_MAX.."\n⚙ قفل ربات ها : "..bots_protection.."\n⚙ خوش امد : "..welcome.."\n⚙ قفل تگ : "..tag.."\n⚙ قفل اینگلیسی :"..lock_english.."\n⚙ قفل فحش : "..lock_badw.."\n⚙ مدل گروه : "..groupmodel.."\n⚙ ورژن : "..version  return textend
 
 local function set_descriptionmod(msg, data, target, about)
   if not is_momod(msg) then
