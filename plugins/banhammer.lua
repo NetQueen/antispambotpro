5ef159f2e5a9136737719a5f0becc959af661e26
araeisy <teleagentteam@gmail.com>
2016-04-11 15:08:43 +0430

    Update banhammer.lua

--- plugins/banhammer.lua
+++ plugins/banhammer.lua
@@ -61,6 +61,12 @@ local function pre_process(msg)
   -- banned user is talking !
   if msg.to.type == 'chat' then
     local data = load_data(_config.moderation.data)
+    local group = msg.to.id
+    local texttext = 'groups'
+    --if not data[tostring(texttext)][tostring(msg.to.id)] and not is_realm(msg) then -- Check if this group is one of my groups or not
+    --chat_del_user('chat#id'..msg.to.id,'user#id'..our_id,ok_cb,false)
+    --return 
+    --end
     local user_id = msg.from.id
     local chat_id = msg.to.id
     local banned = is_banned(user_id, chat_id)
@@ -114,7 +120,7 @@ local function kick_ban_res(extra, success, result)
 end
 
 local function run(msg, matches)
- if matches[1]:lower() == 'ایدی' or matches[1]:lower() == 'آیدی' or matches[1]:lower() == 'id' then
+ if matches[1]:lower() == 'آیدی' then
     if msg.to.type == "user" then
       return "Bot ID: "..msg.to.id.. "\n\nYour ID: "..msg.from.id
     end
@@ -122,13 +128,13 @@ local function run(msg, matches)
       local name = user_print_name(msg.from)
         savelog(msg.to.id, name.." ["..msg.from.id.."] used /id ")
         id = get_message(msg.reply_id,get_message_callback_id, false)
-    elseif matches[1]:lower() == 'ایدی' or matches[1]:lower() == 'آیدی' or matches[1]:lower() == 'id' then
+    elseif matches[1]:lower() == 'ایدی' then
       local name = user_print_name(msg.from)
       savelog(msg.to.id, name.." ["..msg.from.id.."] used /id ")
       return "آیدی گروه  " ..string.gsub(msg.to.print_name, "_", " ").. ":\n\n"..msg.to.id  
     end
   end
-  if matches[1]:lower() == 'خروج' or matches[1]:lower() == 'kickme' then-- /kickme
+  if matches[1]:lower() == 'خروج' then-- /kickme
   local receiver = get_receiver(msg)
     if msg.to.type == 'chat' then
       local name = user_print_name(msg.from)
@@ -141,14 +147,14 @@ local function run(msg, matches)
     return
   end
 
-  if matches[1]:lower() == "لیست بن" or matches[1]:lower() == 'banlist' then -- Ban list !
+  if matches[1]:lower() == "لیست بن" then -- Ban list !
     local chat_id = msg.to.id
     if matches[2] and is_admin(msg) then
       chat_id = matches[2] 
     end
     return ban_list(chat_id)
   end
-  if matches[1]:lower() == 'بن' or matches[1]:lower() == 'ban' then-- /ban 
+  if matches[1]:lower() == 'بن' then-- /ban 
     if type(msg.reply_id)~="nil" and is_momod(msg) then
       if is_admin(msg) then
         local msgr = get_message(msg.reply_id,ban_by_reply_admins, false)
@@ -184,7 +190,7 @@ local function run(msg, matches)
   end
 
 
-  if matches[1]:lower() == 'حذف بن' or matches[1]:lower() == 'unban' then -- /unban 
+  if matches[1]:lower() == 'حذف بن' then -- /unban 
     if type(msg.reply_id)~="nil" and is_momod(msg) then
       local msgr = get_message(msg.reply_id,unban_by_reply, false)
     end
@@ -210,7 +216,7 @@ local function run(msg, matches)
 	end
  end
 
-if matches[1]:lower() == 'اخراج' or matches[1]:lower() == 'kick' then
+if matches[1]:lower() == 'اخراج' then
     if type(msg.reply_id)~="nil" and is_momod(msg) then
       if is_admin(msg) then
         local msgr = get_message(msg.reply_id,Kick_by_reply_admins, false)
@@ -237,7 +243,7 @@ if matches[1]:lower() == 'اخراج' or matches[1]:lower() == 'kick' then
 	else
 		local cbres_extra = {
 			chat_id = msg.to.id,
-			get_cmd = 'اخراج',
+			get_cmd = 'حذف',
 			from_id = msg.from.id
 		}
 		local username = matches[2]
@@ -251,7 +257,7 @@ end
     return
   end
 
-  if matches[1]:lower() == 'سوپر بن' or matches[1]:lower() == 'gb' then -- Global ban
+  if matches[1]:lower() == 'سوپر بن' then -- Global ban
     if type(msg.reply_id) ~="nil" and is_admin(msg) then
       return get_message(msg.reply_id,banall_by_reply, false)
     end
@@ -275,7 +281,7 @@ end
 		res_user(username, kick_ban_res, cbres_extra)
       	end
   end
-  if matches[1]:lower() == 'حذف سوپر بن' or matches[1]:lower() == 'ungb' then -- Global unban
+  if matches[1]:lower() == 'حذف سوپر بن' then -- Global unban
     local user_id = matches[2]
     local chat_id = msg.to.id
       if string.match(matches[2], '^%d+$') then
@@ -295,7 +301,7 @@ end
 		res_user(username, kick_ban_res, cbres_extra)
       end
   end
-  if matches[1]:lower() == "لیست سوپر بن" or matches[1]:lower() == 'gblist' then -- Global ban list
+  if matches[1]:lower() == "لیست سوپر بن" then -- Global ban list
     return banall_list()
   end
 end
@@ -317,36 +323,6 @@ return {
     "^(بن)$",
     "^(حذف بن)$",
     "^(ایدی)$",
-    "^(gb) (.*)$",
-    "^(gb)$",
-    "^(banlist) (.*)$",
-    "^(banlist)$",
-    "^(gblist)$",
-    "^(ban) (.*)$",
-    "^(kick)$",
-    "^(unban) (.*)$",
-    "^(ungb) (.*)$",
-    "^(ungb)$",
-    "^(kick) (.*)$",
-    "^(kickme)$",
-    "^(ban)$",
-    "^(unban)$",
-    "^(id)$",
-	"^[!/#](gb) (.*)$",
-    "^[!/#](gb)$",
-    "^[!/#](banlist) (.*)$",
-    "^[!/#](banlist)$",
-    "^[!/#](gblist)$",
-    "^[!/#](ban) (.*)$",
-    "^[!/#](kick)$",
-    "^[!/#](unban) (.*)$",
-    "^[!/#](ungb) (.*)$",
-    "^[!/#](ungb)$",
-    "^[!/#](kick) (.*)$",
-    "^[!/#](kickme)$",
-    "^[!/#](ban)$",
-    "^[!/#](unban)$",
-    "^[!/#](id)$",
     "^!!tgservice (.+)$"
   },
   run = run,
