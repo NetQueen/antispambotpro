632429fbd4332a123d819d0028a4dde959fba3ac
araeisy <teleagentteam@gmail.com>
2016-04-22 13:53:35 +0430

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -26,13 +26,13 @@ local function list_all_plugins(only_enabled)
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
     --  ☑️ enabled, 🔘 disabled
-    local status = '🔘'
+    local status = '🚫'
     nsum = nsum+1
     nact = 0
     -- Check if is enabled
     for k2, v2 in pairs(_config.enabled_plugins) do
       if v == v2..'.lua' then 
-        status = '☑️' 
+        status = '✔️' 
       end
       nact = nact+1
     end
