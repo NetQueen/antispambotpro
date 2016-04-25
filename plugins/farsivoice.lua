2ebc17deb46da11b5d9e2a7d90fb1eb12ad9bfaf
araeisy <teleagentteam@gmail.com>
2016-04-25 14:27:35 +0430

    Create farsivoice.lua

---
+++ plugins/farsivoice.lua
@@ -0,0 +1,18 @@
+local function run(msg, matches)
+  local eq = matches[1]
+
+  local url = "http://www.farsireader.com/PlayText.aspx?Text="..eq.."Punc=false"
+  local receiver = get_receiver(msg)
+  local file = download_to_file(url,'text.ogg') send_audio('chat#id'..msg.to.id, file, ok_cb , false)
+end
+
+return {
+  description = "Convert text to voice",
+  usage = {
+    "Fa [text]: Convert text to voice"
+  },
+  patterns = {
+    "^[Ff]a (.+)$"
+  },
+  run = run
+}
