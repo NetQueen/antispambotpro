4c8d3704dce955b58e43339d2f5aee9a868527f3
araeisy <teleagentteam@gmail.com>
2016-05-04 19:08:50 +0430

    Update servers.lua

--- plugins/servers.lua
+++ plugins/servers.lua
@@ -2,18 +2,50 @@ do
     
 function run(msg, matches)
   if matches[1] == "reboot" and is_sudo(msg) then
-        local s = io.popen("sh ./data/com/upstart.sh") 
-        return ( s:read("*a") ) 
+        return 'This plugin is disable'  -- Dont enable it,if you use Free online servers!
   elseif matches[1] == "serverinfo" and is_sudo(msg) then
-     local f = io.popen("sh ./data/com/cmd.sh") 
-     return ( f:read("*a") ) 
+                      -- change this location if you use another source/location !
+     local f = io.popen("sh /root/aliraeisy/data/server/serverinfo.sh") 
+     return ( f:read("*a") ) --read all of the results and return it !
   end
 end
 return {
   patterns = {
-    "^(reboot)",
-    "^(serverinfo)"
+    "^[/!](reboot)",
+    "^[/!](serverinfo)",
   },
   run = run
 }
 end
+
+--  -_-_-_-_-_-_-_-_-_-   ||-_-_-_-_-_   ||             ||-_-_-_-_-_
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||-_-_-_-_-_   ||             ||-_-_-_-_-_
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||-_-_-_-_-_   ||-_-_-_-_-_   ||-_-_-_-_-_
+--
+--
+--                               /\                              /\             /-_-_-_-_-_    ||-_-_-_-_-_   ||-_-_-_-_-_
+--  ||\\            //||        //\\        ||      //||        //\\           //              ||             ||         //
+--  || \\          // ||       //  \\       ||     // ||       //  \\         //               ||             ||       //
+--  ||  \\        //  ||      //    \\      ||    //  ||      //    \\       ||                ||             ||    //
+--  ||   \\      //   ||     //______\\     ||   //   ||     //______\\      ||      -_-_-_-   ||-_-_-_-_-_   || //
+--  ||    \\    //    ||    //        \\    ||  //    ||    //        \\     ||           ||   ||             ||  \\ 
+--  ||     \\  //     ||   //          \\   || //     ||   //          \\     \\          ||   ||             ||     \\
+--  ||      \\//      ||  //            \\  ||//      ||  //            \\     \\-_-_-_-_-||   ||-_-_-_-_-_   ||        \\
+--
+--
+--  ||-_-_-_-    ||           ||           ||               //-_-_-_-_-_-
+--  ||     ||    ||           ||           ||              //
+--  ||_-_-_||    ||           ||           ||             //
+--  ||           ||           ||           ||             \\
+--  ||           ||           \\           //              \\
+--  ||           ||            \\         //               //
+--  ||           ||-_-_-_-_     \\-_-_-_-//    -_-_-_-_-_-//
+--
+--By @ali_ghoghnoos
+--@telemanager_ch
